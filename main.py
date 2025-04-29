4b1b81d753e7d79289a06838d7b758f88d62c56a
Fernando Castro <devfercastro@gmail.com>
2025-04-29 19:54:02 -0300

    L5: Cell Class
    - Add `Cell` class to draw cells in the canvas ising `Line` instances
    - Add simple test on main that draws two cells

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from shapes import Line
+from shapes import Cell, Point
 from window import Window
 
 # windows dimentions
@@ -9,9 +9,11 @@ def main():
     # create a window
     win = Window(WIDTH, HEIGHT)
 
-    # draw a black line
-    line = Line()
-    win.draw_line(line, "black")
+    # draw some cells with black lines
+    cell1 = Cell((Point(2, 2), Point(100, 100)), win)
+    cell1.draw()
+    cell2 = Cell((Point(2, 102), Point(100, 202)), win)
+    cell2.draw()
 
     # wait for the window to close
     win.wait_for_close()
--- shapes.py
+++ shapes.py
@@ -1,6 +1,9 @@
+import pdb
 from tkinter import Canvas
 
 from typing_extensions import Literal
+
+from window import Window
 
 
 class Point:
@@ -29,3 +32,56 @@ class Line:
         canvas.create_line(
             self.a.x, self.a.y, self.b.x, self.b.y, fill=fill_color, width=2
         )
+
+
+class Cell:
+    def __init__(
+        self,
+        coordinates: tuple[Point, Point] = (Point(0, 0), Point(10, 10)),
+        window: Window = None,
+    ):
+        if window is None:
+            raise Exception("Cell instance must have a declared 'Window' instance")
+
+        # It should know which walls it has, know where it exists on the canvas in x/y coordinates and have access to the window so that it can draw itself
+        self.walls = (True, True, True, True)  # Order left, top, right, bottom
+        self.coordinates = coordinates
+        self._win = window
+
+    def draw(self):
+        # It takes the x/y coordinates of its top-left corner, and its bottom-right corner as input.
+        top_left_corner, bottom_right_corner = self.coordinates
+
+        lines = []
+        # If the cell has a left wall, draw it.
+        if self.walls[0]:
+            pdb.set_trace()
+            left_line = Line(
+                Point(top_left_corner.x, top_left_corner.y),
+                Point(top_left_corner.x, bottom_right_corner.y),
+            )
+            lines.append(left_line)
+        # If the cell has a top wall, draw it, and so on and so forth for each wall
+        if self.walls[1]:
+            top_line = Line(
+                Point(top_left_corner.x, top_left_corner.y),
+                Point(bottom_right_corner.x, top_left_corner.y),
+            )
+            lines.append(top_line)
+
+        if self.walls[2]:
+            right_line = Line(
+                Point(bottom_right_corner.x, top_left_corner.y),
+                Point(bottom_right_corner.x, bottom_right_corner.y),
+            )
+            lines.append(right_line)
+
+        if self.walls[3]:
+            bottom_line = Line(
+                Point(top_left_corner.x, bottom_right_corner.y),
+                Point(bottom_right_corner.x, bottom_right_corner.y),
+            )
+            lines.append(bottom_line)
+
+        for line in lines:
+            self._win.draw_line(line, "black")
